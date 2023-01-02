a1273eb5781028c4497deb7ed61e365306177493
Jordan R. Willis <jwillis0720@gmail.com>
2023-01-02 13:19:02 -0600

    updating main

--- components/Main.tsx
+++ components/Main.tsx
@@ -2,18 +2,19 @@ import React from "react";
 import Image from "next/image";
 const Main = () => {
     return (
-        <div className="w-full h-full text-center">
-            <div className='max-w-[1240px] w-full h-full object-bottom mx-auto pt-[10rem] flex justify-center items-center'>
-                <div>
-                    <Image
-                        src="logo.svg"
-                        alt="The Fullstack Scientist"
-                        width="750"
-                        height="750"
-                        className="fill-none object-bottom"
-                    />
-
-                </div>
+        <div className='grid md:grid-cols-6 gap-1 pt-[10rem]'>
+            <div className="col-span-3">
+                <Image
+                    src="logo.svg"
+                    alt="The Fullstack Scientist"
+                    width="1000"
+                    height="750"
+                    className="fill-none object-bottom"
+                />
+            </div>
+            <div className="col-span-3 m-auto pr-10">
+                <h2 className="text-black text-center">Hi, my name is <span className="text-green-400">Jordan</span></h2>
+                <h3 className="text-black text-center pt-4 text-2xl">I am the fullstack scientist, trained in both computational and experimental science! This is my portfolio</h3>
             </div>
         </div>
     )
