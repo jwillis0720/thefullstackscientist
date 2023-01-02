2f03b9e1e0e76259112d7987bcdb033cc6ec28cb
Jordan R. Willis <jwillis0720@gmail.com>
2023-01-01 19:15:22 -0600

    one more try with main trying

--- components/Main.tsx
+++ components/Main.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import Image from "next/image";
 const Main = () => {
     return (
-        <div className="w-full h-screen text-center">
-            <div className='max-w-[1240px] w-full h-full  mx-auto pt-[10rem] flex justify-center items-center'>
+        <div className="w-full h-full text-center">
+            <div className='max-w-[1240px] w-full h-full object-bottom mx-auto pt-[10rem] flex justify-center items-center'>
                 <div>
                     <Image
                         src="logo.svg"
                         alt="The Fullstack Scientist"
                         width="750"
                         height="750"
-                        className="fill-none"
+                        className="fill-none object-bottom"
                     />
 
                 </div>
