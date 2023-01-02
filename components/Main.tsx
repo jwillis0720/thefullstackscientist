6d2ded34fd100dd8b37a8d2f2e65310b190705cb
Jordan R. Willis <jwillis0720@gmail.com>
2023-01-02 13:39:48 -0600

    new main resizing

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Image from "next/image";
+import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
 const Main = () => {
     return (
         <div className='grid md:grid-cols-6 gap-2 pt-[10rem]'>
-            <div className="col-span-2">
+            <div className="col-span-1 md:col-span-2">
                 <Image
                     src="logo.svg"
                     alt="The Fullstack Scientist"
@@ -12,9 +15,23 @@ const Main = () => {
                     className="fill-none object-bottom"
                 />
             </div>
-            <div className="col-span-4 m-auto pr-10">
+            <div className="col-span-1 md:col-span-4 m-auto pr-10">
                 <h2 className="text-black text-center">Hi, my name is <span className="text-green-400">Jordan</span></h2>
                 <h3 className="text-black text-center pt-4 text-2xl">I am the fullstack scientist, trained in both computational and experimental science! This is my portfolio</h3>
+                <div className='flex items-center justify-between px-[10rem] pt-5 w-full m-auto'>
+                    <div className='rounded-full text-white bg-slate-700 shadow-lg shadow-gray-400 p-3 text-center cursor-pointer hover-scale:105 ease-in  duration-300'>
+                        <FaLinkedin size={25} />
+                    </div>
+                    <div className='rounded-full text-white shadow-lg bg-slate-700 shadow-gray-400 p-3 text-center cursor-pointer hover-scale:105 ease-in  duration-300'>
+                        <FaGithub size={25} />
+                    </div>
+                    <div className='rounded-full text-white shadow-lg bg-slate-700 shadow-gray-400 p-3 text-center cursor-pointer hover-scale:105 ease-in  duration-300'>
+                        <AiOutlineMail size={25} />
+                    </div>
+                    <div className='rounded-full text-white bg-slate-700 shadow-lg shadow-gray-400 p-3 text-center cursor-pointer hover-scale:105 ease-in  duration-300'>
+                        <BsFillPersonLinesFill size={25} />
+                    </div>
+                </div>
             </div>
         </div>
     )
