6972aeba6282fbe1f0bc8504c83c693f580fdaae
Jordan R. Willis <jwillis0720@gmail.com>
2023-01-04 09:57:41 -0600

    links do stuff

--- components/Main.tsx
+++ components/Main.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
+import Link from "next/link";
 const Main = () => {
     return (
         <div id='home' className='grid md:grid-cols-6 gap-0 pt-[10rem]'>
@@ -21,31 +22,30 @@ const Main = () => {
                 <h3 className="text-black text-center py-4 text-2xl">A fullstack scientist</h3>
                 <p className="text-center max-w-[70%] m-auto text-xl"> I'm trained in both computational and experimental science! My expertise is data science, synthetic biology and deep learning and their application to <span className="text-green-400">antibody</span> and <span className='text-green-400'>vaccine</span> engineering</p>
                 <div className='flex items-center justify-between px-[10rem] pt-7 w-full m-auto'>
-                    <a
-                        href='https://www.linkedin.com/in/jwillis0720'
-                        target='_blank'
-                        rel='noreferrer'
-                    >
+                    <Link href='https://www.linkedin.com/in/jwillis0720'
+                        target='_blank'>
 
                         <div className='rounded-full text-white bg-slate-700 shadow-lg shadow-gray-400 p-3 text-center cursor-pointer hover-scale:105 ease-in  duration-300'>
                             <FaLinkedin size={25} />
                         </div>
-                    </a>
-                    <a
-                        href='https://www.github.com/jwillis0720'
-                        target='_blank'
-                        rel='noreferrer'
-                    >
+                    </Link>
+                    <Link href='https://www.github.com/jwillis0720'
+                        target='_blank'>
                         <div className='rounded-full text-white shadow-lg bg-slate-700 shadow-gray-400 p-3 text-center cursor-pointer hover-scale:105 ease-in  duration-300'>
                             <FaGithub size={25} />
                         </div>
+                    </Link>
+                    <a
+                        href='mailto:jordan@jordanrwillis.com'>
+                        <div className='rounded-full text-white shadow-lg bg-slate-700 shadow-gray-400 p-3 text-center cursor-pointer hover-scale:105 ease-in  duration-300'>
+                            <AiOutlineMail size={25} />
+                        </div>
                     </a>
-                    <div className='rounded-full text-white shadow-lg bg-slate-700 shadow-gray-400 p-3 text-center cursor-pointer hover-scale:105 ease-in  duration-300'>
-                        <AiOutlineMail size={25} />
-                    </div>
-                    <div className='rounded-full text-white bg-slate-700 shadow-lg shadow-gray-400 p-3 text-center cursor-pointer hover-scale:105 ease-in  duration-300'>
-                        <BsFillPersonLinesFill size={25} />
-                    </div>
+                    <Link href='https://scholar.google.com/citations?user=Vxreil0AAAAJ&hl=en' target='_blank'>
+                        <div className='rounded-full text-white bg-slate-700 shadow-lg shadow-gray-400 p-3 text-center cursor-pointer hover-scale:105 ease-in  duration-300'>
+                            <BsFillPersonLinesFill size={25} />
+                        </div>
+                    </Link>
                 </div>
             </div>
         </div >
