27e8aaf401097ebfa7fef42c6f1181664de542ca
Jordan R. Willis <jwillis0720@gmail.com>
2023-01-04 09:32:04 -0600

    min h 8 rem

--- components/skill.tsx
+++ components/skill.tsx
@@ -9,7 +9,7 @@ interface SkillProps {
 
 function Skill(props: SkillProps) {
     return (
-        <div className='p-4 min-h-[10rem] shadow-xl rounded-xl hover:scale-105 hover:bg-green-400 ease-in duration-300 bg-slate-600'>
+        <div className='p-4 min-h-[8rem] shadow-xl rounded-xl hover:scale-105 hover:bg-green-400 ease-in duration-300 bg-slate-600'>
             <div className='grid grid-cols-2 gap-1 justify-center items-center'>
                 <div className='m-auto'>
                     <Image src={props.src} width='100' height='100' alt='/' />
